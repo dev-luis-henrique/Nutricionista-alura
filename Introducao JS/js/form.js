b342de3dfa80168ec3eb9b90ba3f9083eae86bb8
dev-luis-henrique <105171274+dev-luis-henrique@users.noreply.github.com>
2022-06-30 20:43:49 -0300

    Add files via upload

---
+++ Introducao JS/js/form.js
@@ -0,0 +1,49 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click",function(event){
+    event.preventDefault(); // Evita o padrão de um botão label de recarregar a página
+
+    var form = document.querySelector("#form-adiciona");
+    // Extrair informações do paciente.
+    var paciente = obtemPacienteDoFormulario(form);
+    // Criar a Td do paciente.
+    var pacienteTr = montaTr(paciente);
+    // Adicionando o paciente na tabela.
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+    form.reset();
+})
+
+function obtemPacienteDoFormulario(form){
+
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+}
+
+function montaTr(paciente){
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    pacienteTr.appendChild(nomeTd = montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(pesoTd = montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(alturaTd = montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(gorduraTd = montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(imcTd = montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+function montaTd(dado,classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td;
+}
